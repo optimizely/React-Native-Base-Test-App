93a5e0edbc91633f0d01a1175303aa2d006001fc
wangjoshuah <wangjoshuah@gmail.com>
2017-09-30 14:53:29 -0700

    make a home screen and show it on app launch

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Home from './src/screens/Home';
+
+const HomeScreen = StackNavigator({
+  Settings: {
+    screen: Home,
+    navigationOptions: {
+      header: null,
+      headerBackTitle: 'headerBackTitle'
+    }
+  }
+}, {
+  headerMode: 'screen'
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <HomeScreen />
     );
   }
 }
---
+++ src/screens/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import HeaderButton from '../components/HeaderButton';
+
+export default class Home extends Component {
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          What would you like to test?
+        </Text>
+        <Button
+          // onPress{() => navigate('someScreen')}
+          title="APIs"
+        />
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 20,
+    marginVertical: 20,
+  },
+});
